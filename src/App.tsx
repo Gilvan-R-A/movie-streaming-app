a957b6ce32ed243eb4f5677c79fd3438f809fabe
Gilvan-R-A <gilvan.dev@gmail.com>
2025-03-09 16:00:59 -0300

    feat: footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
  export function App() {
    return (
       <Router>
         <Navbar />
         <AppRoutes />
+        <Footer />
       </Router>
    ) 
  }
