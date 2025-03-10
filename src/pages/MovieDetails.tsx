21aca3a6bd05d2d36fb027fc14e4d0fe643dbb89
Gilvan-R-A <gilvan.dev@gmail.com>
2025-03-10 20:04:11 -0300

    style: new styles applied to movie details page

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieDetails, getMovieVideos } from "../services/api";
 import { useFavorites } from "../hooks/useFavorites";
+import styles from "./MovieDetails.module.css";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 
 export function MovieDetails() {
@@ -32,25 +34,33 @@ export function MovieDetails() {
     }
 
     return (
-        <div>
-            <h1>{movie.title}</h1>
-            <p>{movie.overview}</p>
+        <div className={styles.container}>
+            <div className={styles.movieDetails}>
+                <img 
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                    alt={movie.title} 
+                    className={styles.poster} 
+                    />
 
-            <button onClick={() => toggleFavorite(movie)}>
-                {isFavorite(movie.id) ? "Remover dos Favoritos" : "Adicionar aos Favoritos"}
-            </button>
+                    <div className={styles.details}>
+                        <h1>{movie.title}</h1>
+                        <p>{movie.overview}</p>
+
+                        <button onClick={() => toggleFavorite(movie)} className={styles.favoriteButton}>
+                            {isFavorite(movie.id) ? <FaHeart size={20} /> : <FaRegHeart size={20} />}
+                            {isFavorite(movie.id) ? "Remover dos Favoritos" : "Adicionar aos Favoritos"}
+                        </button>
+                    </div>
+            </div>
 
             {videoKey ? (
-                <div>
+                <div className={styles.trailer}>
                     <h2>Trailer</h2>
-                    <iframe 
-                        width="100%" 
-                        height="400" 
-                        src={`https://www.youtube.com/embed/${videoKey}`} 
-                        title="Trailer do Filme" 
-                        style={{border: "none"}} 
-                        allowFullScreen
-                        ></iframe>
+                    <iframe src={`https://youtube.com/embed/${videoKey}`} 
+                    title="Trailer do Filme" 
+                    allowFullScreen
+                    ></iframe>
+
                 </div>
             ) : (
                 <p>Trailer não disponível.</p>
